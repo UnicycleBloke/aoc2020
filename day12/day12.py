f4ea4558b0cb5a933f2a115a8bab97492f7239a6
Alan Chambers <unicycle.bloke@gmail.com>
2020-12-12 10:55:19 +0000

    Added more colourful visualisation as MP4.

--- day12/day12.py
+++ day12/day12.py
@@ -8,18 +8,56 @@ import math
 import turtle
 
 
+class HSVWheel:
+    def __init__(self):
+        self.r = 255
+        self.g = 0
+        self.b = 0
+        self.s = 0
+
+    def next(self):
+        if self.s == 0:
+            self.g += 1
+            if self.g == 255: 
+                self.s = 1
+        if self.s == 1:
+            self.r -= 1
+            if self.r == 0: 
+                self.s = 2
+        if self.s == 2:
+            self.b += 1
+            if self.b == 255: 
+                self.s = 3
+        if self.s == 3:
+            self.g -= 1
+            if self.g == 0: 
+                self.s = 4
+        if self.s == 4:
+            self.r += 1
+            if self.r == 255: 
+                self.s = 5
+        if self.s == 5:
+            self.b -= 1
+            if self.b == 0: 
+                self.s = 0
+
+        return (self.r, self.g, self.b)    
+
+
+
 def part1(data):
     x = 0
     y = 0
     a = 90
 
+    h = HSVWheel()
     w = turtle.getscreen()    
     t = turtle
 
+    t.colormode(255)
     t.width(6)
     t.pencolor('blue')
     t.dot()
-    #t.speed(0)
     input()
 
     t.width(3)
@@ -28,6 +66,8 @@ def part1(data):
     t.shape('square')
 
     for d in data:
+        t.pencolor(h.next()) 
+
         if (d[0] == 'N'): 
             y = y + d[1]
             t.goto(x, y)
@@ -61,27 +101,32 @@ def part1(data):
     return int(abs(x) + abs(y) + 0.001)  
 
 
+
 def part2(data):
     x  = 10
     y  = 1
     sx = 0
     sy = 0 
-
+    
+    h = HSVWheel()
     w = turtle.getscreen()
     
     t = turtle
     t.hideturtle()
     t.width(6)
+    t.colormode(255)
     t.pencolor('blue')
     t.dot()
-    #t.speed(0)
+    t.speed(0)
+    input()
 
     t.width(3)
-    t.pencolor('red')
     t.fillcolor('green')
     t.shape('square')
 
     for d in data:
+        t.pencolor(h.next())
+
         if (d[0] == 'N'): 
             y = y + d[1]
 
@@ -111,8 +156,13 @@ def part2(data):
         if (d[0] == 'F'):
             sx = sx + d[1] * x
             sy = sy + d[1] * y
-            t.goto(sx/100, sy/100)
+            t.goto(sx / 30, sy / 30)
 
+    t.width(6)
+    t.pencolor('red')
+    t.dot()    
+
+    input()
     return int(abs(sx) + abs(sy) + 0.001)  
 
 
@@ -127,8 +177,8 @@ def solution(file):
     p1 = part1(dirs)
     print("Part1: ", p1)
 
-    #p2 = part2(dirs)
-    #print("Part2: ", p2)
+    p2 = part2(dirs)
+    print("Part2: ", p2)
 
 
 def main():
