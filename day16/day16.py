2b0d0a5108d50b0ca0dd677e7dc6478fe76540f6
Alan Chambers <alan.chambers@cambridgeconsultants.com>
2020-12-16 13:01:22 +0000

    Finally found the bug by talking to a duck.

--- day16/day16.py
+++ day16/day16.py
@@ -5,86 +5,68 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import utils.utils as ut
 from copy import deepcopy
 import math
+from functools import reduce
 
-
-primes = ut.read_primes()
-
-
-def valid(fields, ticket):
+def invalid_sum(fields, ticket, extra):
     total = 0
-    for i in ticket:
+    for p in ticket:
         valid = False
         for f in fields:
-            if int(i) >= int(f[1]) and int(i) <= int(f[2]): valid = True
-            if int(i) >= int(f[3]) and int(i) <= int(f[4]): valid = True
+            if p >= f[1] and p <= f[2]: valid = True
+            if p >= f[3] and p <= f[4]: valid = True
         if valid == False:
-            total += int(i)
-            #print(i)
+            total += (p + extra)
+            # Perfect for Part 1, massive bug in Part 2 due to 0 
+            # value in some ticket being invalid. So add on an extra bit to fix.
+            #total += p
     return total
+
+
+def field_match(f, p):
+    if p >= f[1] and p <= f[2]: return True
+    if p >= f[3] and p <= f[4]: return True
+    return False
 
 
 def part1(fields, ticket, tickets):
     total = 0
     for t in tickets:
-        total += valid(fields, t)
+        total += invalid_sum(fields, t, 0)
     return total
 
 
-def field_valid_for_ticket(f, t, i):
-    #print('Test', f, t, i)
-    if int(t[i]) >= int(f[1]) and int(t[i]) <= int(f[2]): return True
-    if int(t[i]) >= int(f[3]) and int(t[i]) <= int(f[4]): return True
-    return False
-
-
-def field_valid_for_tickets(f, ts, i):
-    count = 0
-    for t in ts:
-        if field_valid_for_ticket(f, t, i) == True: count += 1
-    return count
-
-
 def part2(fields, ticket, tickets):
-    good = []
-    for t in tickets:
-        if valid(fields, t) == 0:
-            good.append(t)
+    valid_tickets = [t for t in tickets if invalid_sum(fields, t, 1) == 0]
 
-    # Valid tickets
-    for g in good:
-        print(g)   
-    print(len(good))
-
-    # Fields with ranges
+    # For each field, find the set of positions for which it matches all 
+    # valid tickets - this is a rule
+    rules = []
     for f in fields:
-        print(f)   
-    print(len(fields))     
+        # The field potentially matches all positions for all tickets
+        positions = {p for p in range(len(ticket))}
+        for t in valid_tickets:
+            # Only some positions match this field for this ticket
+            match = {p for p in range(len(ticket)) if field_match(f, t[p]) == True }
+            positions = positions.intersection(match)
+        rules.append(positions)
 
-    # Which fields match all valid tickets in position i
-    # ERROR: Most positions have several matches
-    # ERROR: Some positions have none
-    rules = {}
-    for i in range(len(ticket)):
-        count = []
-        for f in fields:
-            if field_valid_for_tickets(f, good, i) == len(good):
-                #rules[i] = f
-                count.append(f[0])
-                #break
-        print(i, count)
+    # Iterate to find the positions for each field - if the rule has 
+    # one entry, that is a match.
+    result  = 1
+    result2 = set()
+    found   = set()
+    for k in range(len(ticket)):
+        for i in range(len(rules)):
+            if len(rules[i]) == 1:
+                if fields[i][0].startswith('departure'):
+                    p = list(rules[i])[0]
+                    result = result * ticket[p]
+                    result2.add(p)
+                    #print(k, p, ticket[p], fields[i][0], result, result2)
+                found = found.union(rules[i]) 
 
-    for r in rules:            
-        print(r, rules[r])
-    print(len(rules))     
-
-    print(ticket, len(ticket))
-    result = 1
-    for r in rules:
-        #print(r, rules[r], rules[r][0])
-        if rules[r][0].startswith('departure'):
-            #print(r, ticket[r])
-            result = result * int(ticket[r])
-
+        for j in range(len(rules)):
+            rules[j] = rules[j] - found
 
     return result
 
@@ -94,39 +76,31 @@ def solution(file):
     lines = ut.read_lines(file)
 
     i = 0
-    # Rules
     fields = []
     while len(lines[i]) > 0:
         m = re.match('^(.*):\s(\d+)-(\d+)\sor\s(\d+)-(\d+)$', lines[i])
         if m == None:
             break 
-        fields.append([m.group(x) for x in range(1, 6)])
-        #print([m.group(x) for x in range(1, 6)])
+        field = [m.group(1)] + [int(m.group(x)) for x in range(2, 6)]
+        fields.append(field)
         i += 1
 
     i += 1
-    # Ticket
-    ticket = lines[i].split(',')
-    #print(ticket)
+    ticket = [int(x) for x in lines[i].split(',')]
     i += 2
 
-    # Tickets
     tickets = []
     while i < len(lines) > 0:
-        tickets.append(lines[i].split(','))
-        #print(lines[i].split(','))
+        tickets.append([int(x) for x in lines[i].split(',')])
         i += 1
 
-    #p1 = part1(fields, ticket, tickets)
-    #print("Part1: ", p1)
-
+    p1 = part1(fields, ticket, tickets)
+    print("Part1: ", p1)
     p2 = part2(fields, ticket, tickets)
     print("Part2: ", p2)
 
 
 def main():
-    #solution("day16-test.txt")
-    #solution("day16-test2.txt")
     solution("day16-input.txt")
 
 
