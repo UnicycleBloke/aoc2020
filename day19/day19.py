dde0d82781a9ca30e9dd87364f0206a3c65e3eb9
Alan Chambers <unicycle.bloke@gmail.com>
2020-12-19 16:24:31 +0000

    Added memoisation to Day 19 - didn't do much, but the cache was useful for Part 2

--- day19/day19.py
+++ day19/day19.py
@@ -4,7 +4,9 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import utils.utils as ut
 
 
-def rule_matches(rules, index):
+def rule_matches(rules, memo, index):
+    if index in memo: return memo[index]
+
     rule = rules[index]
     if rule[0] == 0:
         return [rule[1]]
@@ -12,11 +14,13 @@ def rule_matches(rules, index):
         result = []
         for group in rule[1]:
             indices = [int(x) for x in group.split()]
-            res = rule_matches(rules, indices[0])
+            res = rule_matches(rules, memo, indices[0])
             for i in indices[1:]:
-                parts = rule_matches(rules, i)
+                parts = rule_matches(rules, memo, i)
                 res  = [r + p for r in res for p in parts]
             result = result + res    
+        
+        memo[index] = result
         return result
     return None
 
@@ -46,14 +50,14 @@ def matches_rule8(prefixes, suffixes, message):
     return False
 
 
-def part1(rules, messages):
-    ok = rule_matches(rules, 0)
+def part1(rules, memo, messages):
+    ok = rule_matches(rules, memo, 0)
     return sum([1 for m in messages if m in ok])
 
 
-def part2(rules, messages):
-    prefixes = rule_matches(rules, 42)
-    suffixes = rule_matches(rules, 31)
+def part2(rules, memo, messages):
+    prefixes = rule_matches(rules, memo, 42)
+    suffixes = rule_matches(rules, memo, 31)
     return sum([1 for m in messages if matches_rule8(prefixes, suffixes, m)])
 
 
@@ -70,19 +74,19 @@ def solution(file):
         if len(r[1]) == 4:
             rules[int(r[0])] = [0, r[1][2]]
         else:
-            rules[int(r[0])] = [1, r[1].split('|')]
+            rules[int(r[0])] = [1, [x.strip() for x in r[1].split('|')]]
 
     messages = data[i+1:]
 
-    p1 = part1(rules, messages)
+    memo = {}
+    p1 = part1(rules, memo, messages)
     print("Part1: ", p1)
-
-    p2 = part2(rules, messages)
+    p2 = part2(rules, memo, messages)
     print("Part2: ", p2)
 
 
 def main():
-    solution("day19-test.txt")
+    #solution("day19-test.txt")
     solution("day19-input.txt")
 
 
